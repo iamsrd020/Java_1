7949187147dbdbaf331f61d1191d55bee56360b4
DARSHAN S R <darshansr403@gmail.com>
2026-09-03 23:37:02 +0530

    String Methods

---
+++ src/StringMethodsArch.java
@@ -0,0 +1,37 @@
+/*
+String
+ │
+ ├── Find information
+ │     ├── length()
+ │     ├── charAt()
+ │     ├── indexOf()
+ │     └── lastIndexOf()
+ │
+ ├── Compare
+ │     ├── equals()
+ │     ├── equalsIgnoreCase()
+ │     ├── compareTo()
+ │     └── compareToIgnoreCase()
+ │
+ ├── Modify / create new String
+ │     ├── toUpperCase()
+ │     ├── toLowerCase()
+ │     ├── replace()
+ │     ├── replaceAll()
+ │     ├── trim()
+ │     └── concat()
+ │
+ └── Convert
+       ├── split()
+       ├── toCharArray()
+       └── valueOf()
+*/
+
+public class StringMethodsArch {
+
+    public static void main(String[] args) {
+
+        // Your String practice code goes here
+
+    }
+}
