84789b968ab92d868105e6d6b474d403951c276c
DARSHAN S R <darshansr403@gmail.com>
2026-09-03 13:15:40 +0530

    Java learning from one_Depth

---
+++ src/Company.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Company {
+    public static void main(String[] args){
+        String[] employees = {"Darshan", "Harshitha", "D boss"};
+
+        System.out.println(employees[0]);
+        System.out.println(employees[1]);
+        System.out.println("I live in Bangalore");
+        System.out.println(employees.length);
+        System.out.println(Arrays.toString(employees));
+    }
+}
