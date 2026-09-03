c37507d0d596f6973d0dd721bdd2fe3115db15db
DARSHAN S R <darshansr403@gmail.com>
2026-09-03 15:29:12 +0530

    Palindrome code

---
+++ src/Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+
+    public static void main(String[] args){
+
+        String name = "MOM";
+        String reversed = "";
+
+        for(int i = name.length() - 1; i >= 0; i--) {
+            reversed = reversed + name.charAt(i);
+        }
+        if (name.equals(reversed)){
+            System.out.println("Palindrome || True");
+        }else {
+            System.out.println("Not palindrome || False");
+        }
+    }
+}
